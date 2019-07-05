40ff05526fb1341f1e56c670e8b668b63fe0a224
vruello <hello@vruello.fr>
2019-07-05 22:54:00 +0200

    Add bytes->u64 in variable integers and some tests

--- src/variable_integer.rs
+++ src/variable_integer.rs
@@ -1,3 +1,16 @@
+use std::fmt;
+use std::error::Error;
+
+#[derive(Debug, Clone)]
+struct ArrayTooLargeError();
+
+impl fmt::Display for ArrayTooLargeError{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "array length must not be larger than 9.")
+    }
+}
+
+impl Error for ArrayTooLargeError {}
 
 pub struct VariableInteger {
     integer: u64
@@ -33,5 +46,59 @@ impl VariableInteger {
             bytes.extend_from_slice(&self.integer.to_le_bytes()[..8]);
             bytes
         }
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Result<u64, Box<dyn Error>> {
+        if bytes.len() > 9 {
+            return Result::Err(Box::new(ArrayTooLargeError()));
+        }
+        let first_byte = bytes[0] as u64;
+        if first_byte < 0xFD {
+            Result::Ok(first_byte)
+        } else {
+            let mut nbytes = [0; 8];
+            for (i, byte) in bytes[1..].iter().enumerate() {
+                nbytes[i] = *byte;
+            }
+            Result::Ok(u64::from_le_bytes(nbytes))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn test(number: u64) {
+        let vi = VariableInteger::new(number);
+        assert_eq!(number, VariableInteger::from_bytes(&vi.bytes()).unwrap());
+    }
+
+    #[test]
+    fn test_8() {
+        test(0x42);
+    }
+
+    #[test]
+    fn test_16() {
+        test(0xFAFE);
+    }
+
+    #[test]
+    fn test_32() {
+        test(0xFAFBFCFD);
+    }
+
+    #[test]
+    fn test_64() {
+        test(0xFAFBFCFDFEFF);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_array() {
+        let arr = [0 as u8; 10];
+        VariableInteger::from_bytes(&arr).unwrap();
     }
 }
